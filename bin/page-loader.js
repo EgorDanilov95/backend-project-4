180fb2ff53c994be840e5283f10ba5132a5a25e2
Egor Danilov <danilov-egor-e@yandex.ru>
2025-12-10 07:13:00 +0000

    prepare the project

---
+++ bin/page-loader.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+import { Command } from 'commander'
+import pageLoader from '../src/page-loader.js'
+
+const program = new Command()
+
+program
+  .name('page-loader')
+  .description('Page loader utility')
+  .version('1.0.0')
+  .argument('<url>', 'URL to download')
+  .option('-o, --output <dir>', 'output directory', process.cwd())
+  .action((url, options) => {
+    pageLoader(url, options.output)
+    .then(filepath => console.log(filepath))
+    .catch(error => {
+      console.error('Error:', error.message);
+      process.exit(1);
+    })
+  })
+
+program.parse()
---
+++ src/page-loader.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+import axios from 'axios'
+import fs from 'fs'
+import path from 'path'
+import transformUrl from './url-to-filename.js';
+const { promises: fsp } = fs
+const getData = (url) => {
+    return axios.get(url)
+    .then(response => response.data)
+    .catch(error => {
+        console.log(error)
+        throw error
+    })
+}
+
+const pageLoader = (url, outputDir = process.cwd()) => {
+   return getData(url)
+    .then(html => {
+        let fileName = transformUrl(url)
+        let fullPath = path.join(outputDir,fileName)
+        return fsp.writeFile(fullPath, html, 'utf-8').then(() => fullPath)
+        })
+        .catch(error => {
+            console.log(error)
+            throw error
+    })
+}
+export default pageLoader
+
