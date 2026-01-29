f7f0692ff38d70806e75f72f33f3f3d95c463b06
Egor Danilov <danilov-egor-e@yandex.ru>
2026-01-29 12:33:11 +0000

    added debuger

---
+++ src/logger.js
@@ -0,0 +1,18 @@
+import debug from 'debug'
+
+
+export const log = debug('page-loader');
+export const logNetwork = debug('page-loader:network');
+export const logFile = debug('page-loader:file');
+export const logError = debug('page-loader:error');
+export const logResource = debug('page-loader:resource');
+export const logDebug = debug('page-loader:debug');
+
+log.color = '32';       // зелёный
+logNetwork.color = '36'; // голубой
+logFile.color = '33';    // жёлтый
+logError.color = '31';   // красный
+logResource.color = '35'; // пурпурный
+logDebug.color = '90';    // серый
+
+
--- src/page-loader.js
+++ src/page-loader.js
@@ -5,25 +5,41 @@ import path from 'path'
 import { transformUrl, getResourcesDirname, getResourceFilename } from './url-to-filename.js'
 import { extractResources, replaceResourceSources } from './html-processor.js'
 import downloadResource from './resource-handler.js'
+import { log, logNetwork, logFile, logError, logResource, logDebug } from './logger.js'
 
 const { promises: fsp } = fs
 
 const getData = (url) => {
+  logNetwork('Начинаю запрос к %s', url)
   return axios.get(url)
-    .then(response => response.data)
+    .then(response => {
+      logNetwork('Успешный ответ от %s, статус: %d, размер: %d байт', 
+        url, response.status, response.data.length)
+      return response.data})
     .catch((error) => {
+      logError('Ошибка при запросе к %s: %O', url, error)
       throw error
     })
 }
 
 const pageLoader = (url, outputDir = process.cwd()) => {
+   const startTime = Date.now()
+  log('🚀 Начинаю загрузку страницы')
+  log('URL: %s', url)
+  log('Директория для сохранения: %s', outputDir)
   return getData(url)
     .then((html) => {
       let pageFileName = transformUrl(url)
       let pagePath = path.join(outputDir, pageFileName)
-
+      logFile('Имя файла для HTML: %s', pageFileName)
+      logFile('Полный путь: %s', pagePath)
       const resources = extractResources(html, url)
-
+  log('Найдено ресурсов: %d', resources.length)
+  logDebug('Ресурсы: %O', resources.map(r => ({
+        tag: r.tagName,
+        src: r.originalSrc,
+        fullUrl: r.url
+      })))
       if (resources.length === 0) {
         return fsp.writeFile(pagePath, html, 'utf-8')
           .then(() => pagePath)
@@ -31,15 +47,22 @@ const pageLoader = (url, outputDir = process.cwd()) => {
 
       const resourcesDirname = getResourcesDirname(pageFileName)
       const resourceDir = path.join(outputDir, resourcesDirname)
+      logFile('Создаю директорию для ресурсов: %s', resourceDir)
 
       return fsp.mkdir(resourceDir, { recursive: true })
         .then(() => {
+          logFile('Директория создана: %s', resourceDir)
           const downloadPromises = resources.map((resource) => {
             const resourceFilename = getResourceFilename(resource.url)
             const resourcePath = path.join(resourceDir, resourceFilename)
             resource.localPath = path.join(resourcesDirname, resourceFilename)
+
+            logResource('Скачиваю ресурс: %s', resource.url)
+            logResource('Сохраняю как: %s', resourcePath)
+
             return downloadResource(resource.url, resourcePath)
           })
+          log('Начинаю скачивание %d ресурсов...', resources.length)
           return Promise.all(downloadPromises)
         })
         .then(() => {
@@ -49,14 +72,24 @@ const pageLoader = (url, outputDir = process.cwd()) => {
             originalSrc: resource.originalSrc,
             newSrc: resource.localPath
           }))
+          log('Заменяю ссылки в HTML...')
           const modifiedHtml = replaceResourceSources(html, replacements)
           return fsp.writeFile(pagePath, modifiedHtml, 'utf-8')
         })
-        .then(() => pagePath)
-    })
+        .then(() => {
+          const totalTime = Date.now() - startTime
+          log('✅ Загрузка завершена успешно!')
+          log('📊 Итоги:')
+          log('   Страница: %s', url)
+          log('   Сохранённый HTML: %s', pagePath)
+          log('   Ресурсов: %d', resources.length)
+          log('   Время выполнения: %dms', totalTime)
+          return pagePath
+        })
     .catch((error) => {
       throw error
     })
+  })
 }
 
 export default pageLoader
